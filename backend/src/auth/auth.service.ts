02de6e9ade5c32343d44f48b785447ad76048456
Fadwa <fadwa.mahmoud@gmail.com>
2026-02-25 03:25:42 +0200

    feat(backend): implement auth signin and jwt token issuance

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,11 +1,14 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import * as argon2 from 'argon2';
 import { UsersService } from '../users/users.service';
 import { SignupDto } from './dto/signup.dto';
+import { SigninDto } from './dto/signin.dto';
+import { JwtPayload } from './types/jwt-payload';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService) { }
+    constructor(private readonly usersService: UsersService, private readonly jwtService: JwtService,) { }
 
     async signup(dto: SignupDto) {
         const email = dto.email.toLowerCase().trim();
@@ -34,5 +37,28 @@ export class AuthService {
             if (err?.code === 11000) throw new ConflictException('Email already in use');
             throw err;
         }
+    }
+    async signin(dto: SigninDto) {
+        const email = dto.email.toLowerCase().trim();
+
+        const user = await this.usersService.findByEmail(email);
+        if (!user) {
+            // Don't leak if email exists
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const ok = await argon2.verify(user.passwordHash, dto.password);
+        if (!ok) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const payload: JwtPayload = {
+            sub: user._id.toString(),
+            email: user.email,
+        };
+
+        const accessToken = await this.jwtService.signAsync(payload);
+
+        return { accessToken };
     }
 }
